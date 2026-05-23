000d715dd5c47048e71d6e9b6d471e8dc5ca5b1e
Atinderbir <atinderbir3@gmail.com>
2026-05-23 15:24:42 +0530

    escape sequence characters

---
+++ escape-sequence-character/main.py
@@ -0,0 +1,19 @@
+# Dont remove this line
+print('Hello, \n how are you?')
+
+# Multiline comment
+
+print('hello this is also a character \'hello\' \n this is second line')
+
+'''
+This is a multiline comment. It can be used to write comments that span multiple lines.
+It is enclosed in triple quotes.
+'''
+
+# to join logs with a characters use sep='character'
+print('hey', 5, 6, sep='-') 
+
+# to end a line with a character use end='character'
+print('hey', end='116')
+print('hey', end='116\n')
+print('hey')
