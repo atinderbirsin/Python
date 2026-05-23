aa73a7336aad077d38aa6d5a41b27c3e5ab0cfd9
Atinderbir <atinderbir3@gmail.com>
2026-05-23 15:58:28 +0530

    type casting

---
+++ type-casting/main.py
@@ -0,0 +1,15 @@
+# type casting :- it is the process of converting one data type to another data type.
+
+#  int() , flat(), hex(), oct(), str() etc. are the built-in functions for type casting.
+a = int('1')
+# a = 1
+b = int('2')
+# b = 2
+print(a + b) # addition
+
+# Implicit type casting
+
+c = 1.5
+d = 8
+
+print(c + d) # addition type float
