44c6c8fce089ff61b4e88f729caf5b94d5a7526f
Atinderbir <atinderbir3@gmail.com>
2026-05-23 15:38:13 +0530

    variables and data types

---
+++ variables-data-types/main.py
@@ -0,0 +1,30 @@
+a = 483789239
+print(a)
+b = "Atinder"
+print(b)
+
+# 4 types of variables
+e = 1
+f = 'Atinder'
+g = True
+h = None 
+
+print(type(e), type(f), type(g), type(h))
+
+# list :- list of different data types
+
+lis1 = [1, 2, 'Atinder', True, 5];
+print(lis1)
+print(type(lis1))
+
+# tuple :- tuple of different data types but it is immutable
+
+tup1 = (1, 2, 'Atinder', True, 5);
+print(tup1)
+print(type(tup1)) 
+
+#  dictionary :- it is a collection of key value pairs
+
+dict1 = {'name': 'Atinder', 'age': 25, 'city': 'Jalandhar'}
+print(dict1)
+print(type(dict1))
